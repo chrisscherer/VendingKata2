4893e663789ae65f425481115e0e899f35a83c87
chrisscherer <chrisscherer90@gmail.com>
2016-09-26 03:15:56 -0400

    first push

---
+++ Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VendingKataTake2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VendingKataTake2Tests
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
