347d82f23fda092f7647e5b963c240e472d681ff
chrisscherer <chrisscherer90@gmail.com>
2016-09-26 03:52:55 -0400

    Green light on amount update test

--- Application/VendingMachine.cs
+++ Application/VendingMachine.cs
@@ -4,11 +4,14 @@ namespace VendingKataTake2
 {
 	public class VendingMachine
 	{
+		public int Amount { get; private set; }
+
 		public bool InsertCoin (int coinSize)
 		{
 			if (coinSize <= 0)
 				return false;
-			
+
+			Amount += coinSize;
 			return true;
 		}
 
