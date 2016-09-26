cc5618a3cc248306d7bf31e1a283b913ab823bd2
chrisscherer <chrisscherer90@gmail.com>
2016-09-26 03:26:56 -0400

    Set up the framework for the first test.

---
+++ Tests/VendingTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace VendingKataTake2Tests
+{
+	[TestFixture]
+	class VendingMachineTests
+	{
+		[SetUp] public void Init ()
+		{
+		}
+
+		[TearDown] public void Dispose ()
+		{
+		}
+
+		[Test]
+		public void Test_Name ()
+		{
+		}
+	}
+}
