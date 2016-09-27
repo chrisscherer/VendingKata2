474ef82df0bef24d7cd75859d93ae927c3397afb
chrisscherer <chrisscherer90@gmail.com>
2016-09-27 05:57:23 -0400

    Added coin class to refactor for proper coin use.

---
+++ Application/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VendingKataTake2
+{
+	public class Coin
+	{
+		public int Weight { get; private set; }
+
+		public int Size { get; private set; }
+
+		public Coin (int size, int weight)
+		{
+			Weight = weight;
+			Size = size;
+		}
+	}
+}
+
